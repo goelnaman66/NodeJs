9a1f421a5b34b19d8b5e4ebb642e7f95a049a99f
Naman Goel <goelnaman66@gmail.com>
2022-02-04 01:42:55 +0530

    Step 3: User Queries implemented in usermodelimpl file and implemented in main file

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-var Mongoose = require('mongoose');
-var User = require('./user');
+const Mongoose = require('mongoose');
+const User = require('./user')
+const UserQuery = require('./userModelImpl');
 
-const connect = ()=>{
-    return Mongoose.connect('mongodb://localhost:27017/UnacademyMongoDemo')
-}
+
+const connect = () =>{
+   return Mongoose.connect('mongodb://localhost:27017/UnacademyMongoDemo');
+};
+
 
 connect()
-.then( async connection=>{
-    console.log("Mongo Db connection setup Done!!")
+.then(async connection => {
+    console.log('Mongo DB Connection Setup Done!!');
 
-    //Just for demo we would delete the document
+    // Just For Demo
     await User.deleteMany({});
-    
+    // await Blog.deleteMany({});
+    //
 
-    const user= await User.create({
-        firstName: 'Naman',
-        lastName: 'Goel',
-        email: 'goelnaman66@gmail.com'
-    })
-    console.log(user);
+    const user = await User.create({
+        firstName: 'Sanket',
+        lastName: 'Singh',
+        email: 'ss@gmail.com'
+    });
+    console.timeLog(user);
 
-
-    //creating multiple users
     await User.create([
         {
-            firstName: 'Shubh',
-            lastName: 'Goyal',
-            email: 'gg@gmail.com'
-        },
+            firstName: 'Sarthak',
+            lastName: 'Jain',
+            email: 'sj@gmail.com'
+        }, 
         {
-            firstName: 'Rashmi',
-            lastName:'Goel',
-            email:"hgdd@gmail.com"
+            firstName: 'Upkar',
+            email: 'u@gmail.com',
+            underAge: true
         }
     ])
+
+    
+    console.log("******")
+    const u1 = await UserQuery.getAllUsers();
+    const u2 = await UserQuery.getUserById(user.id);
+    const u3 = await UserQuery.updateUserByID(user.id, {underAge: true});
+    console.log(u1);
+    console.log(u2);
+    console.log(u3);
+    console.log("*********************************");
+
 })
 
 .catch(err=> console.log(err))
