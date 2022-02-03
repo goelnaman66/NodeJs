5a09a900a8e55905bfd7952a1de0a4071061e0da
Naman Goel <goelnaman66@gmail.com>
2022-02-04 00:42:17 +0530

    Step 2: Creating user schema and creating few users

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 var Mongoose = require('mongoose');
+var User = require('./user');
 
 const connect = ()=>{
     return Mongoose.connect('mongodb://localhost:27017/UnacademyMongoDemo')
 }
 
 connect()
-.then(connection=>{
+.then( async connection=>{
     console.log("Mongo Db connection setup Done!!")
+
+    //Just for demo we would delete the document
+    await User.deleteMany({});
+    
+
+    const user= await User.create({
+        firstName: 'Naman',
+        lastName: 'Goel',
+        email: 'goelnaman66@gmail.com'
+    })
+    console.log(user);
+
+
+    //creating multiple users
+    await User.create([
+        {
+            firstName: 'Shubh',
+            lastName: 'Goyal',
+            email: 'gg@gmail.com'
+        },
+        {
+            firstName: 'Rashmi',
+            lastName:'Goel',
+            email:"hgdd@gmail.com"
+        }
+    ])
 })
+
 .catch(err=> console.log(err))
